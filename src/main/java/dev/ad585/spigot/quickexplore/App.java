8ce0568d8afdc7e49fbb52e18954c0804fab6bcb
David Parks <david.parks2@my.uu.edu>
2020-05-07 14:43:57 -0500

    initial commit

---
+++ src/main/java/dev/ad585/spigot/quickexplore/App.java
@@ -0,0 +1,20 @@
+package dev.ad585.spigot.quickexplore;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends JavaPlugin
+{
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
---
+++ src/test/java/dev/ad585/spigot/quickexplore/AppTest.java
@@ -0,0 +1,20 @@
+package dev.ad585.spigot.quickexplore;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
