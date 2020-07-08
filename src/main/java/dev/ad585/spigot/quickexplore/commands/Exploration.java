eef0ca73f718f4a107189e74f6b2e27e76cb28ef
David <davidparks11@gmail.com>
2020-07-08 14:23:02 -0400

    returns player back to call location after wait

--- src/main/java/dev/ad585/spigot/quickexplore/commands/Exploration.java
+++ src/main/java/dev/ad585/spigot/quickexplore/commands/Exploration.java
@@ -2,18 +2,24 @@ package dev.ad585.spigot.quickexplore.commands;
 
 import dev.ad585.spigot.quickexplore.Main;
 import dev.ad585.spigot.quickexplore.util.LocationUtil;
+import net.md_5.bungee.api.ChatColor;
 
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitTask;
+
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.bukkit.Location;
 
 public class Exploration implements CommandExecutor {
     private Main plugin;
     private final double MAX_DISTANCE = 20000;
     private final double MIN_DISTANCE = 800;
+    private int taskTime = 20;
+    private HashMap<UUID, Location> playerLocations = new HashMap<UUID, Location>();
 
     public Exploration(Main plugin) {
         this.plugin = plugin;
@@ -31,10 +37,17 @@ public class Exploration implements CommandExecutor {
         }
 
         Player p = (Player) sender;
-        
+
         // check permission
         if (p.hasPermission("explore.use")) {
-            Location sourceLocation = p.getLocation();
+            if (playerLocations.containsKey(p.getUniqueId())) {
+                p.sendMessage(ChatColor.RED
+                        + "You are already exploring! Complete the current task or type \"/explore quit\"");
+                return false;
+                // }
+            } else {
+                playerLocations.put(p.getUniqueId(), p.getLocation());
+            }
             Location targetLocation = LocationUtil.getNextRandLocation(p.getLocation(), MIN_DISTANCE, MAX_DISTANCE);
             if (LocationUtil.isFloorDangerous(targetLocation) || !LocationUtil.inWorldBorder(targetLocation)) {
                 p.sendMessage("Sorry! That location would've been dangerous. Try again!");
@@ -44,14 +57,23 @@ public class Exploration implements CommandExecutor {
                 p.teleport(targetLocation);
                 String message = "You've been sent to " + targetLocation.toString();
                 p.sendMessage(message);
-
-                BukkitTask returnTimer = p.getServer().getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+                p.getServer().getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+                    @Override
                     public void run() {
-                        p.teleport(sourceLocation);
+                        try {
+                            Thread.sleep(taskTime * 1000);
+                        } catch (InterruptedException e) {
+                            plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+                                p.sendMessage(ChatColor.RED
+                                        + "Something went wrong! We're sending you back. Be sure to contact an Admin");
+                            });
+                        }
+                        plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+                            p.teleport(playerLocations.get(p.getUniqueId()));
+                        });
                     }
                 });
-                
-                
+
                 return true;
             }
         } else {
